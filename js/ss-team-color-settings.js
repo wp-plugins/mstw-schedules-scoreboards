93a8d50b70b7bd8b7870ca3ec237e4884555176e
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 04:23:46 +0000

    Added colors to TEAM CPT. UI uses color picker via this script.
    git-svn-id: https://plugins.svn.wordpress.org/mstw-schedules-scoreboards/trunk@1110038 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/ss-team-color-settings.js
@@ -0,0 +1,12 @@
+//Set up the color pickers to work with our text input field
+jQuery(document).ready(function($){
+
+	$('#team_primary_bkgd_color').wpColorPicker();
+	
+	$('#team_primary_text_color').wpColorPicker();
+	
+	$('#team_accent_color_1').wpColorPicker();
+	
+	$('#team_accent_color_2').wpColorPicker();
+	
+});
